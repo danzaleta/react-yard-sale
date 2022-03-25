c10254e908865d02d4d134405dfa22406e328ee2
Daniel Zaleta <zaletadaniel@gmail.com>
2022-03-24 19:22:23 -0600

    Header

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,23 +1,41 @@
-import React from 'react'
-import '../styles/Login.scss'
+import React from "react";
+import "../styles/Login.scss";
 
 const Login = () => {
   return (
     <div className="login">
-        <div className="form-container">
-            <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo"/>
-            <h1 className="title">Create a new password</h1>
-            <p className="subtitle">Enter a new password for your account</p>
-            <form action="/" className="main-form">
-                <label htmlFor="password" className="label">Password</label>
-                <input type="password" placeholder="********" id="password" className="input input-password"/>
-                <label htmlFor="new-password" className="label">Re-enter password</label>
-                <input type="password" placeholder="********" id="new-password" className="input input-password"/>
-                <input type="submit" value="Confirm" className="primary-button login-button"/>
-            </form>
-        </div>
+      <div className="form-container">
+        <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+        <h1 className="title">Create a new password</h1>
+        <p className="subtitle">Enter a new password for your account</p>
+        <form action="/" className="main-form">
+          <label htmlFor="password" className="label">
+            Password
+          </label>
+          <input
+            type="password"
+            placeholder="********"
+            id="password"
+            className="input input-password"
+          />
+          <label htmlFor="new-password" className="label">
+            Re-enter password
+          </label>
+          <input
+            type="password"
+            placeholder="********"
+            id="new-password"
+            className="input input-password"
+          />
+          <input
+            type="submit"
+            value="Confirm"
+            className="primary-button login-button"
+          />
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default Login;
