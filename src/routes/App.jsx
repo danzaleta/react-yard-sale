34b8ffd94eb9fef4493fe48182da998d27f69e41
Daniel Zaleta <zaletadaniel@gmail.com>
2022-03-27 11:14:29 -0600

    All routes on App.jsx

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -15,6 +15,12 @@ const App = () => {
           <Route path="/" element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/recovery-password" element={<RecoveryPassword/>}/>
+          <Route path="/send-email" element={<SendEmail/>}/>
+          <Route path="/new-password" element={<NewPassword/>}/>
+          <Route path="/account" element={<MyAccount/>}/>
+          <Route path="/signup" element={<CreateAccount/>}/>
+          <Route path="/checkout" element={<Checkout/>}/>
+          <Route path="/orders" element={<Orders/>}/>
           <Route path='*' element={<NotFound/>}/>
         </Routes>
       </Layout>
