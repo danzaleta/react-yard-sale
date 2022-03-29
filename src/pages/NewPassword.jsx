a109a5007c324433e9e60d8e9f4b11d0085c0c47
Daniel Zaleta <zaletadaniel@gmail.com>
2022-03-28 18:59:02 -0600

    React design: New password window

---
+++ src/pages/NewPassword.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import '../styles/NewPassword.scss'
+
+const NewPassword = () => {
+    return (
+        <div className="NewPassword">
+            <div className="NewPassword-container">
+                <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+                <h1 className="title">Create a new password</h1>
+                <p className="subtitle">Enter a new password for your account</p>
+
+                <form action="/" className="main-form">
+                    <label htmlFor="password" className="label">
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        placeholder="********"
+                        id="password"
+                        className="input input-password"
+                    />
+
+                    <label htmlFor="new-password" className="label">
+                        Re-enter password
+                    </label>
+                    <input
+                        type="password"
+                        placeholder="********"
+                        id="new-password"
+                        className="input input-password"
+                    />
+
+                    <input
+                        type="submit"
+                        value="Confirm"
+                        className="primary-button login-button"
+                    />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default NewPassword;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import Home from '../pages/Home';
 import Login from '../containers/Login';
 import PasswordRecovery from '../pages/PasswordRecovery';
 import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
 import NotFound from '../pages/NotFound';
 import '../styles/global.css';
 
@@ -13,12 +14,13 @@ const App = () => {
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/password-recovery" element={<PasswordRecovery/>}/>
-          <Route path="/send-email" element={<SendEmail/>}/>
-          
-          <Route path='*' element={<NotFound/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/password-recovery" element={<PasswordRecovery />} />
+          <Route path="/send-email" element={<SendEmail />} />
+          <Route path="/new-password" element={<NewPassword />} />
+
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Layout>
     </BrowserRouter>
