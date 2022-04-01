c6bcc5de4a80f0ce8d73006a498fbbb20526b80e
Daniel Zaleta <zaletadaniel@gmail.com>
2022-04-01 11:38:41 -0600

    Product list with API request using hook useEffect

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ProductItem from '../components/ProductItem';
 import '@styles/ProductList.scss'
+import axios from 'axios';
+
+//const API = process.env.API;
+const API = 'https://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  
+  useEffect(async () => {
+    const response = await axios(API);
+    setProducts(response.data);
+    console.log(response.data);
+  }, [])
+
   return (
     <section className="main-container">
-        <div className="ProductList">
-            <ProductItem/>
-            <ProductItem/>
-            <ProductItem/>
-            <ProductItem/>
-            <ProductItem/>
-            <ProductItem/>
-            <ProductItem/>
-            <ProductItem/>
-        </div>
+      <div className="ProductList">
+        {products.map(product => (
+          <ProductItem/>
+        ))}
+      </div>
     </section>
   );
 }
