bac34a8241903671a54e96d3044870a2562a90f7
Daniel Zaleta <zaletadaniel@gmail.com>
2022-04-01 11:37:55 -0600

    Toggle menu with hook useState

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
-import React from "react";
-import '@styles/Header.scss';
-
-import menu from '@icons/icon_menu.svg';
-import logo from '@logos/logo_yard_sale.svg';
-import shoppingCart from '@icons/icon_shopping_cart.svg';
+import React, { useState } from "react";
+import "@styles/Header.scss";
+import Menu from "@components/Menu";
+import menu from "@icons/icon_menu.svg";
+import logo from "@logos/logo_yard_sale.svg";
+import shoppingCart from "@icons/icon_shopping_cart.svg";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <nav>
       <img src={menu} alt="menu" className="menu" />
@@ -34,13 +39,16 @@ const Header = () => {
       </div>
       <div className="navbar-right">
         <ul>
-          <li className="navbar-email">user@email.com</li>
+          <li className="navbar-email" onClick={handleToggle}>
+            user@email.com
+          </li>
           <li className="navbar-shopping-cart">
             <img src={shoppingCart} alt="shopping cart" />
             <div>2</div>
           </li>
         </ul>
       </div>
+      {toggle && <Menu />}
     </nav>
   );
 };
