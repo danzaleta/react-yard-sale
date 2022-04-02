1d8439bbc72790a6369cc830abe1a6b9033cf39a
Daniel Zaleta <zaletadaniel@gmail.com>
2022-04-02 11:33:12 -0600

    useRef hook for login window

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,43 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 import "@styles/Login.scss";
-
 import logo from '@logos/logo_yard_sale.svg';
 
+
+
 const Login = () => {
+  const form = useRef(null);
+  const handleSubmit = () => {
+    const formData = new FormData(form.current);
+    const data = {
+      username: formData.get('email'),
+      password: formData.get('password')
+    }
+    console.log(data);
+  };
+
   return (
-    <div className="login">
+    <div className="Login">
       <div className="form-container">
         <img src={logo} alt="logo" className="logo" />
-        <h1 className="title">Create a new password</h1>
-        <p className="subtitle">Enter a new password for your account</p>
-        <form action="/" className="main-form">
-          <label htmlFor="password" className="label">
-            Password
-          </label>
-          <input
-            type="password"
-            placeholder="********"
-            id="password"
-            className="input input-password"
-          />
-          <label htmlFor="new-password" className="label">
-            Re-enter password
-          </label>
-          <input
-            type="password"
-            placeholder="********"
-            id="new-password"
-            className="input input-password"
-          />
-          <input
-            type="submit"
-            value="Confirm"
-            className="primary-button login-button"
-          />
+        <form action="/" className="main-form" ref={form}>
+          <label htmlFor="email" className="label">Email address</label>
+          <input type="text" placeholder="user@email.com" name="email" className="input input-email" />
+          <label htmlFor="password" className="label">Password</label>
+          <input type="password" placeholder="********" name="password" className="input input-password" />
+          <input type="button" value="Login" className="primary-button login-button" onClick={handleSubmit} />
+          <a href="/">Forgot my password</a>
+          <button type="button" className="secondary-button signup-button">Sign up</button>
         </form>
       </div>
     </div>
   );
-};
+}
 
 export default Login;
